bb3871fe78b71da3927eec2baffea0046624a412
ibandominguez <ibandominguez@live.com>
2016-10-03 17:17:36 +0100

    test route registering

--- tests/unit/AppTest.php
+++ tests/unit/AppTest.php
@@ -11,4 +11,21 @@ class AppTest extends TestCase
     $this->assertInstanceOf('IbanDominguez\RestUp\App', new App());
   }
 
+  public function testItRegistersRoutes()
+  {
+    $app = new App();
+
+    $app->add('posts', [
+      'fields' => [
+        'title' => ['type' => 'string', 'rules' => 'required|string'],
+        'content' => ['type' => 'string', 'rules' => 'required|string'],
+      ]
+    ]);
+
+    $resources = $app->getResources();
+
+    $this->assertTrue(count($resources) == 1);
+    $this->assertInstanceOf('IbanDominguez\RestUp\Resource', $resources[0]);
+  }
+
 }
