afda0cf8f65f95315750e7f894902c6abdf54ac6
ibandominguez <ibandominguez@live.com>
2016-10-03 17:17:49 +0100

    tests passing

--- src/App.php
+++ src/App.php
@@ -2,4 +2,20 @@
 
 namespace IbanDominguez\RestUp;
 
-class App {}
+class App {
+
+  protected $resources;
+
+  public function add($title, $fields)
+  {
+    $this->resources[] = new Resource($title, $fields);
+
+    return $this;
+  }
+
+  public function getResources()
+  {
+    return $this->resources;
+  }
+
+}
