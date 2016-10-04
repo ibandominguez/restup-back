e51a5e98ebc6a1ba96b7a1762733e889b5e8c3a2
ibandominguez <ibandominguez@live.com>
2016-10-04 15:08:10 +0100

    exeption is thrown

--- src/Route.php
+++ src/Route.php
@@ -4,6 +4,12 @@ namespace IbanDominguez\RestUp;
 
 class Route {
 
-  //
+  public function __construct($name, $path, $method, array $fields)
+  {
+    $this->name = $name;
+    $this->path = $path;
+    $this->method = $method;
+    $this->fields = $fields;
+  }
 
 }
