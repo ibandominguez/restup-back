28e3487a2dec7ac4a70132786a6910042e4d7257
ibandominguez <ibandominguez@live.com>
2016-10-04 15:01:08 +0100

    remove use statements

--- tests/unit/ResourceTest.php
+++ tests/unit/ResourceTest.php
@@ -2,26 +2,21 @@
 
 use PHPUnit\Framework\TestCase;
 use IbanDominguez\RestUp\Resource;
-use InvalidArgumentException;
-use Exception;
 
 class ResourceTest extends TestCase
 {
 
   protected function setUp()
   {
-    set_error_handler(array($this, 'errorHandler'));
-  }
-
-  public function errorHandler($errno, $errstr, $errfile, $errline)
-  {
-    throw new InvalidArgumentException(sprintf(
-      'Missing argument. %s %s %s %s',
-      $errno,
-      $errstr,
-      $errfile,
-      $errline
-    ));
+    set_error_handler(function($errno, $errstr, $errfile, $errline) {
+      throw new InvalidArgumentException(sprintf(
+        'Missing argument. %s %s %s %s',
+        $errno,
+        $errstr,
+        $errfile,
+        $errline
+      ));
+    });
   }
 
   public function testThrowsAnExpetionIfParamsAreInvalid()
