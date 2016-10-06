fb9b7edbfed1ac0736a91d3abebd17f6880e9cfd
ibandominguez <ibandominguez@live.com>
2016-10-06 15:24:38 +0100

    add type to app logic and db squema

--- src/App.php
+++ src/App.php
@@ -104,10 +104,10 @@ class App
     $this->db->exec('CREATE TABLE IF NOT EXISTS fields(
       id INT(11) AUTO_INCREMENT PRIMARY KEY,
       resource_id INT(11),
+      type VARCHAR(50) NOT NULL,
       title VARCHAR(50) NOT NULL,
       value TEXT,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-      updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
     )');
   }
 
