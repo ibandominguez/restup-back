65bbe68bde5f2de4dec706b92043d53cf597eab1
ibandominguez <ibandominguez@live.com>
2016-10-10 13:05:11 +0100

    add example

--- example/index.php
+++ example/index.php
@@ -5,18 +5,23 @@ require __DIR__.'/../vendor/autoload.php';
 use IbanDominguez\RestUp\App;
 
 App::create([
-  'M_DEBUG' => true,
   'DB_HOST' => 'localhost',
   'DB_NAME' => 'prueba',
   'DB_USER' => 'root',
   'DB_PASS' => '',
 ])
+->auth([
+  'admin@email.com' => 'admin'
+])
 ->add('posts', [
   ['title' => 'title', 'type' => 'string', 'rules' => 'required'],
   ['title' => 'body',  'type' => 'string'],
   ['title' => 'date',  'type' => 'date',   'rules' => 'date']
+], [
+  'except' => ['show']
 ])
 ->add('cars', [
-  ['title' => 'brand', 'type' => 'string']
+  ['title' => 'brand', 'type' => 'string', 'rules' => 'required'],
+  ['title' => 'made_at',  'type' => 'datetime']
 ])
 ->run(true);
